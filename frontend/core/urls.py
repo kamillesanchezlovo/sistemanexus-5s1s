6749763e49a4d9dac39305170ecd84fe8a5c6fa4
Kamille Aynara Sánchez Lovo <kamilleaynarasanchez@gmail.com>
2025-11-26 18:24:00 -0600

    Delete frontend directory

--- frontend/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("ping/", views.ping, name="ping"),
-    path("presupuestos/", views.crear_presupuesto, name="crear_presupuesto"),
-    path("inventario/movimientos/", views.movimiento_inventario, name="movimiento_inventario"),
-]
